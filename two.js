965cc4a355f85a15350d86c52b962cee86bc47b8
Sam <hangho.dev@gmail.com>
2023-09-15 15:16:38 +0700

    bai2

---
+++ two.js
@@ -0,0 +1,26 @@
+let trongso = [2, 1, 2];
+let a = 8;
+let b = 9;
+let c = 8;
+let d = "hang A";
+let e = "hang B";
+let f = "hang C";
+let h = "hang F";
+if ((a * 2 + b * 2 + c * 1) / (2 + 2 + 1) >= 9.0) {
+  console.log(d);
+}
+if (
+  (a * 2 + b * 2 + c * 1) / (2 + 2 + 1) >= 7.0 &&
+  (a * 2 + b * 2 + c * 1) / (2 + 2 + 1) < 9.0
+) {
+  console.log(e);
+}
+if (
+  (a * 2 + b * 2 + c * 1) / (2 + 2 + 1) >= 5.0 &&
+  (a * 2 + b * 2 + c * 1) / (2 + 2 + 1) < 7.0
+) {
+  console.log(f);
+}
+if ((a * 2 + b * 2 + c * 1) / (2 + 2 + 1) < 5.0) {
+  console.log(h);
+}
